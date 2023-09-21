bd24ef0b2b97d83af504a8923a1d4e81b19306a3
nasim0994 <nasimuddin0994@gmail.com>
2023-09-21 17:41:04 +0600

    header design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,44 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
-    <header>
-      <div className="container">Header</div>
+    <header className="py-2">
+      <div className="container">
+        <div className="flex justify-between items-center">
+          <Link to="/" className="flex items-center gap-1">
+            <img src="/images/logo/logo.png" alt="" className="w-10" />
+            <h1 className="font-bold text-xl">
+              <span className="text-primary">Max</span>
+              <span className="text-secondary">UI</span>
+            </h1>
+          </Link>
+
+          <nav>
+            <ul className="flex  menu">
+              <li>
+                <Link to="">Home</Link>
+              </li>
+              <li>
+                <Link to="">Products</Link>
+              </li>
+              <li>
+                <Link to="">Components</Link>
+              </li>
+              <li>
+                <Link to="">Pages</Link>
+              </li>
+              <li>
+                <Link to="">Blogs</Link>
+              </li>
+              <li>
+                <Link to="" className="bg-primary-g px-3 py-2.5 text-sm">
+                  Login/Signup
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </header>
   );
 }
--- src/components/HomeComponents/Hero/Hero.jsx
+++ src/components/HomeComponents/Hero/Hero.jsx
@@ -1,28 +1,34 @@
+import Header from "../../Header/Header";
+
 export default function Hero() {
   return (
     <section className="hero_wrap">
-      <div className="container">
-        <div className="grid grid-cols-2">
-          <div>
-            <h1 className="text-5xl font-bold text-neutral-content leading-[55px]">
-              We have created{" "}
-              <span className="text-primary-after relative">incredible</span>{" "}
-              tools to easily design your{" "}
-              <span className="text-secondary-after relative">website</span>.
-            </h1>
+      <Header />
 
-            <div className="mt-6">
-              <button className="bg-primary-g px-4 py-2 rounded">
-                View Products
-              </button>
+      <div className="hero_content">
+        <div className="container">
+          <div className="grid grid-cols-2">
+            <div className="relative z-40">
+              <h1 className="text-5xl font-bold  leading-[55px]">
+                We have created{" "}
+                <span className="text-primary-after relative">incredible</span>{" "}
+                tools to easily design your{" "}
+                <span className="text-secondary-after relative">website</span>.
+              </h1>
+
+              <div className="mt-6 ">
+                <button className="bg-primary-g px-4 py-2 rounded">
+                  View Products
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="absolute right-0 top-1/2 -translate-y-1/2 ">
-            <img
-              src="/images/hero/banner_hand.png"
-              alt=""
-              className="w-[500px] 2xl:w-[680px]"
-            />
+            <div className="absolute right-0 top-1/2 -translate-y-1/2 ">
+              <img
+                src="/images/hero/banner_hand.png"
+                alt=""
+                className="w-[500px] 2xl:w-[680px]"
+              />
+            </div>
           </div>
         </div>
       </div>
